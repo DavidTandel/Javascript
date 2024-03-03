f55e481ff4e7aebb41a87f0d3b562a4faea98424
David Tandel <202101251@daiict.ac.in>
2024-03-03 12:06:17 +0530

    Acknowledging variables

---
+++ 01_Basics/variables.js
@@ -0,0 +1,8 @@
+const accountID = 123456
+//accountID = 2345 // You Cant Change const Variable
+
+let accountEmail = "david@google.com"
+var accountPass = "789"
+accountCity = "Ahmedabad"
+
+console.table([accountID , accountEmail , accountPass , accountCity]);
