9ab7aa9a187885449a1d911d8371ddb87c1e078b
David Tandel <202101251@daiict.ac.in>
2024-03-03 18:46:35 +0530

    commit

--- 01_Basics/variables.js
+++ 01_Basics/variables.js
@@ -1,8 +1,9 @@
 const accountID = 123456
 //accountID = 2345 // You Cant Change const Variable
 
-let accountEmail = "david@google.com"
-var accountPass = "789"
+let accountEmail = "david@google.com" // Lets you change variable
+var accountPass = "789" //Scope Problems so dont remember/ Block scope and functional scope
 accountCity = "Ahmedabad"
+let acccountState;
 
-console.table([accountID , accountEmail , accountPass , accountCity]);
+console.table([accountID , accountEmail , accountPass , accountCity , acccountState]);
