73243eaf22084d0bb96cc272dbf391d8184123cb
David Tandel <202101251@daiict.ac.in>
2024-03-03 18:45:41 +0530

    Datatypes

---
+++ 01_Basics/datatypes.js
@@ -0,0 +1,21 @@
+"use strict" // treat all JS code as a newer version code
+
+let name = "David" //string 
+let age = 18 //Number
+let isLoggedIn = true //Boolean
+
+
+
+
+
+//Number Range 2^53
+//bigint
+//string
+//boolean
+//null -> standalonevalue
+//undefined -> 
+//symbol -> Unique
+
+
+//object
+console.log(typeof null)
